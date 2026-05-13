666813eb0e86a70742f7407283b7092f43af529c
dipti <dipti@DIPTIBHATTA>
2026-05-13 16:43:13 +0545

    Add project files.

---
+++ Lab1/Question1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Question1
+    {
+        static void Main()
+        {
+            string? name;
+
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Welcome " + name + ", to dotnet technology");
+        }
+    }
+}
